52c1e1816bdfa6bc8c2dc8c639acde73a05626ec
unknown <nguyendocuongbka@gmail.com>
2020-08-01 18:11:02 +0700

    addbuild

---
+++ build/precache-manifest.1dd5815be3e14a298ff912dbfec50477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbce2d704f6328ccae5f98003ad15c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd83ee5809bcb88e2aed",
+    "url": "/static/css/main.4ef64bf4.chunk.css"
+  },
+  {
+    "revision": "1480089ce8e3c09792d8",
+    "url": "/static/js/2.5653e1dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5653e1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd83ee5809bcb88e2aed",
+    "url": "/static/js/main.c4acab85.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
